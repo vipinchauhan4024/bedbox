7a84edd00b23a584f029db1afede7b2eeaf8d95b
Chauhan Vipin <vipin.chauhan@volvo.com>
2021-05-26 19:52:43 +0530

    Intaial commit

---
+++ src/main/java/com/bedbox/app/bedbox/BedboxApplication.java
@@ -0,0 +1,13 @@
+package com.bedbox.app.bedbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bedbox/app/bedbox/BedboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedbox.app.bedbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
